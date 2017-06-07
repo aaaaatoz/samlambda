10791c83b461f661b42dad2431414e806299f260
aaaaatoz <rafa.xu.au@gmail.com>
2017-06-07 14:23:58 +1000

    add request

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-
-var time = require('time');
+var request = require('request');
 exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
+    var request = require('request');
+    request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    });
     callback(null, {
         statusCode: '200',
         body: 'The time in Los Angeles is: '
